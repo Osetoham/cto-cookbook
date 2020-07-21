d3727186784503ffff763ffa199f22c1dfd2ffb3
Mercy Okobi <mercyokobi@gmail.com>
2020-07-21 14:52:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const itemsLink = document.getElementsByClassName('items-link')[0]
+
+// toggleButton.addEventListener('click', () => {
+//    itemsLink.classList.toggle('active')
+// })
+
+// const hamburger = document.getElementById('hamburger');
+// const itemsLink = document.getElementById('items-link');
+
+// hamburger.addEventListener('click',() =>{
+//     itemsLink.classList.toggle('show');
+// })
